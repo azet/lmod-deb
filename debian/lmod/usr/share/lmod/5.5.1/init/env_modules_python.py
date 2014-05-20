a0f1873f5c1ea74bcbcc180a2ad2cf4b0dd0cf6d
Aaron Zauner <azet@azet.org>
2014-05-21 00:51:11 +0200

    restored original Makefile.in, added debian/patches, removed build dirs

--- debian/lmod/usr/share/lmod/5.5.1/init/env_modules_python.py
+++
@@ -1,7 +0,0 @@
-# -*- python -*-
-import os, string
-def module(command, *arguments):
-  commands = os.popen('/usr/share/lmod/lmod/libexec/lmod python %s %s'\
-                      % (command, string.join(arguments))).read()
-  exec commands
-
--- debian/lmod/usr/share/lmod/5.5.1/modulefiles/Core/lmod/5.5.1.lua
+++
@@ -1,3 +0,0 @@
--- -*- lua -*-
-whatis("Description: Lmod: An Environment Module System")
-prepend_path('PATH','/usr/share/lmod/lmod/libexec')
